4d4e4b0bd22846f06db6db5371f8c4c413a2d871
Ekin Celebi <ekincelebi@anadolusigorta.com.tr>
2020-11-15 15:47:46 +0300

    update ngram.asm

--- ngram.asm
+++ ngram.asm
@@ -1,39 +1,40 @@
-;segment .bss
-;size1 resd 1
-;size2 resd 1
-;n resd 1
+segment .bss
+num resd 1
+counter resd 1
+
 segment .data
 msg db "hello, world",10
-len equ 14
+
 
 segment .text
 global ngram
 
 ngram:
-    push ebp
-    mov  ebp,esp
+	
+	push ebp
+    	mov  ebp,esp
 
-	;mov dword [n], [ebp+8]
-	;mov dword [size2], [ebp+12]
-	;mov dword [size1], [ebp+20]
-    ;mov eax,4
-    ;mov ebx,1
-    ;mov  ecx,[ebp+16] ;str2
-    mov ecx,msg
+	mov dword [num], [ebp+20] ;num stores str2 len
+	mov dword [counter], 0 ;init counter
+	
+	mov  ecx,[ebp+16] ;str2
+    	mov  edx,[ebp+24] ;n
+    	sub [num], edx ;subtract param n from num
+    	inc dword [num] ;num holds how many ngrams a string holds
     
-    ;mov  edx,[ebp+24] ;str1
-    ;xor  eax,eax
-    ;mov edx,[ebp+12] 
-    mov edx,len
-    int 80
     
-    mov eax,1 
-    mov ebx,0
-    int 80h
-	
-	
-	
-	
-	
-    pop  ebp
-    ret
+loop:
+	inc dword [counter] ;increment counter
+	mov eax,4 ;print by the n count
+    	mov ebx,1
+    	int 80h
+    	inc ecx ;shift ecx by 1 location
+    	cmp counter,[num]
+    	jz finished
+    	jmp loop
+    
+    
+    
+finished:  
+    	pop  ebp
+    	ret
