ee71aac4960377ca9451f771f6696344b78c3fbe
sysprog <geceavsar@gmail.com>
2020-11-17 03:11:03 +0300

    percent values are printed, slight bug fix

--- ngram.asm
+++ ngram.asm
@@ -1,5 +1,4 @@
 
-    
     segment .bss ;declares variables
 temp1 resd 1
 temp2 resd 1
@@ -9,6 +8,8 @@ hit dd 0 ;how many matching ngram
 counter dd 0 ;to check if n letters are matched
 index2 dd 0 ; holds str2 index
 index1 dd 0 ;holds str1 index
+ngram1 dd 0 ;#of ngram in str1
+ngram2 dd 0 ;#of ngram in str2
 
 ;;str1 ebp+8
 ;;size1 ebp+12
@@ -20,12 +21,9 @@ segment .text
 global ngram
 
 ngram:
-	
 	push ebp
     mov  ebp,esp
 
-	;xor eax,eax
-	
 	;init counters
 	mov dword [hit],0
 	mov dword [index1],0
@@ -35,8 +33,20 @@ ngram:
 	
 	mov esi,0 ; esi = 0
 	mov edi,0 ; edi = 0
-	
 	mov ebx, [ebp+24] ;ebx = n 
+	
+	;;calculate #of ngrams for str1
+	mov eax, [ebp+12]
+	sub eax, ebx
+	add eax, 1
+	mov dword [ngram1], eax
+	
+	;;calculate #of ngrams for str2
+	xor eax, eax
+	mov eax, [ebp+20]
+	sub eax, ebx
+	add eax, 1
+	mov dword [ngram2], eax
 	
 l1:
 	mov dword [counter], 0
@@ -44,36 +54,31 @@ l1:
 	mov dword [index2],0
 	
 l4: 	
-	mov byte dl, [edx+edi]
-	mov byte cl, [ecx+esi]
-	cmp dl,cl
-	
+	push eax
+	push ebx
+	mov byte al, [edx+edi]
+	mov byte bl, [ecx+esi]
+	cmp al,bl
+	pop ebx
+	pop eax
 	je match 
 	
-l3:	;inc edx
+l3:	
 	inc edi ;increment offset instead of register
 	add dword [index2],1 ;increment index 2
-	
-	;;;;eax in burda str2 de ilerlemesi lazım
-	mov eax,[ebp+20]; eax = size 2
-	sub eax,ebx ;eax = size2 - n
-	inc eax 
+		
+	mov eax, [ngram2]
 	cmp [index2],eax
-	;;;
-	
+	;;check for if str2 reached end	
 	je l2
 	jmp l4 
 
-l2: ;pop ecx
+l2: 
 	pop esi ;pop esi back 
-	;inc ecx
 	inc esi
 	add dword [index1],1 ;increment index 1
 	
-	;;;;eax in burda str1 de ilerlemesi lazım
-	mov eax,[ebp+12] ;eax = size1
-	sub eax,ebx ;eax = size1 - n
-	inc eax 
+	mov eax, [ngram1]	
 	cmp [index1],eax
 	;;check for if str1 reached end
 	je fin
@@ -88,10 +93,13 @@ match:
 	inc esi
 	inc edi
 	
-	mov byte dl, [edx+edi]
-	mov byte cl, [ecx+esi]
-	cmp dl,cl
-	
+	push eax
+	push ebx
+	mov byte al, [edx+edi]
+	mov byte bl, [ecx+esi]
+	cmp al,bl
+	pop ebx
+	pop eax
 	je match
 	
 	jmp l2 
@@ -101,8 +109,20 @@ addHit:
 	jmp l2
 	
 fin:
-	mov eax,[hit]
+	mov edi, [ngram1]
+	mov esi, [ngram2]
+b1:
+	add edi, esi
+	mov esi, 100
+	mov eax,[hit] ;;eax is intersection
+	xor edx, edx ;;clear edx
+    sub edi, eax ;;edi is union
+    cmp edi, 0
+    je noCalc
     
+	mul esi
+	div edi
+noCalc:	
     pop ebp 
     ret
     
